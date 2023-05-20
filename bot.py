5a842fb3d5732620027d0bbc4ac06eb82e51a92c
nodirbek0701 <nodirbektalabaguldu@gmail.com>
2023-05-20 21:01:24 +0500

    coffebot

--- bot.py
+++ bot.py
@@ -4,17 +4,17 @@ import logging
 from api import get_coffee
 logging.basicConfig(level=logging.INFO)
 
-bot=Bot(token="tokin yozasan")
+bot=Bot(token="6132460317:AAG6IS0Gqp01rBT-lWQk7x9bmD5D7SetYS0")
 dp=Dispatcher(bot)
 
 @dp.message_handler(commands="start")
 async def salomlash(message:types.Message):
-    await message.answer("Salom men senga coffe yuboraman /cofe ni bos")
+    await message.answer("Salom men sizga coffe rasmlarini  yuboraman /cofe ni bosing \nHar bosganizda har hil cofe yuboraman")
 
 @dp.message_handler(commands="cofe")
 async def cofe(message:types.Message):
     rasm=get_coffee()
-    await message.answer(rasm)
+    await message.reply_photo(photo=rasm)
 
 if __name__=="__main__":
     executor.start_polling(dp,skip_updates=True)
